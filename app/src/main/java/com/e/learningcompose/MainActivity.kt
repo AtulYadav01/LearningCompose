9bba2dff11c5ba0e9d2795f57261da66691b42d8
AtulYadav01 <ashuy033@gmail.com>
2021-09-28 17:35:45 +0530

    Learn about Modifiers.

--- app/src/main/java/com/e/learningcompose/MainActivity.kt
+++ app/src/main/java/com/e/learningcompose/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.e.learningcompose
 
+import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Colors
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.e.learningcompose.ui.theme.LearningComposeTheme
@@ -27,15 +30,36 @@ class MainActivity : ComponentActivity() {
                 Column(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .width(300.dp)
+                        .fillMaxWidth()
+                        .padding(40.dp, 60.dp, 80.dp, 100.dp)
+                        .border(5.dp, Color.Red)
                         .background(Color.Green),
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceAround
+                    verticalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Text(text = "Hello", color = Color.White)
-                    Text(text = "Hola", color = Color.White)
-                    Text(text = "Bonjour", color = Color.White)
-                    Text(text = "Dattebayo", color = Color.White)
+                    Text(text = "Hello", modifier = Modifier.clickable {
+                        Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
+                    })
+                    Text(text = "Hola", modifier = Modifier.clickable {
+                        Toast.makeText(this@MainActivity, "Hola", Toast.LENGTH_SHORT).show()
+                    })
+                    Text(
+                        text = "Bonjour",
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(Color.Red)
+                            .border(2.dp, Color.Black)
+                            .padding(20.dp)
+                            .clickable {
+                                Toast
+                                    .makeText(this@MainActivity, "Bonjour", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                    )
+                    Spacer(modifier = Modifier.height(100.dp))
+                    Text(text = "Dattebayo", modifier = Modifier.clickable {
+                        Toast.makeText(this@MainActivity, "Dattebayo", Toast.LENGTH_SHORT).show()
+                    })
                 }
             }
         }
