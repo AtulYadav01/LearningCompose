cd824342169618b8180b648e496ff56fffb12ed4
AtulYadav01 <ashuy033@gmail.com>
2021-09-28 16:53:52 +0530

    Learn about Columns and Rows.

--- app/src/main/java/com/e/learningcompose/MainActivity.kt
+++ app/src/main/java/com/e/learningcompose/MainActivity.kt
@@ -3,21 +3,39 @@ package com.e.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.e.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            MaterialTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(300.dp)
+                        .background(Color.Green),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text(text = "Hello", color = Color.White)
+                    Text(text = "Hola", color = Color.White)
+                    Text(text = "Bonjour", color = Color.White)
+                    Text(text = "Dattebayo", color = Color.White)
                 }
             }
         }
@@ -33,6 +51,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     LearningComposeTheme {
-        Greeting("Atul")
+        Greeting("Android")
     }
 }
