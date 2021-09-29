25df58a47bb4b2261d2e685f6c5ef411d2683c8d
AtulYadav01 <ashuy033@gmail.com>
2021-09-29 14:29:04 +0530

    Learn about Modifiers.

--- app/src/main/java/com/e/learningcompose/MainActivity.kt
+++ app/src/main/java/com/e/learningcompose/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.e.learningcompose
 
-import android.graphics.drawable.shapes.OvalShape
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -13,11 +12,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.e.learningcompose.ui.theme.LearningComposeTheme
@@ -29,37 +24,64 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 Column(
                     modifier = Modifier
+                        .clickable {
+                            Toast
+                                .makeText(this, "Block", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        .background(Color.Green)
                         .fillMaxHeight()
                         .fillMaxWidth()
                         .padding(40.dp, 60.dp, 80.dp, 100.dp)
-                        .border(5.dp, Color.Red)
-                        .background(Color.Green),
+                        .border(5.dp, Color.Red),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Text(text = "Hello", modifier = Modifier.clickable {
-                        Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
-                    })
-                    Text(text = "Hola", modifier = Modifier.clickable {
-                        Toast.makeText(this@MainActivity, "Hola", Toast.LENGTH_SHORT).show()
-                    })
+                    Text(text = "Hello", modifier = Modifier
+                        .clickable {
+                            Toast
+                                .makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        .background(Color.White)
+                        .border(2.dp, Color.Black)
+                        .padding(20.dp)
+                    )
+                    Text(text = "Hola", modifier = Modifier
+
+                        .clickable {
+                            Toast
+                                .makeText(this@MainActivity, "Hola", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        .background(Color.White)
+                        .border(2.dp, Color.Black)
+                        .padding(20.dp)
+                    )
                     Text(
                         text = "Bonjour",
-                        color = Color.White,
+                        color = Color.Black,
                         modifier = Modifier
-                            .background(Color.Red)
-                            .border(2.dp, Color.Black)
-                            .padding(20.dp)
                             .clickable {
                                 Toast
                                     .makeText(this@MainActivity, "Bonjour", Toast.LENGTH_SHORT)
                                     .show()
                             }
+                            .background(Color.White)
+                            .border(2.dp, Color.Black)
+                            .padding(20.dp)
+
                     )
-                    Spacer(modifier = Modifier.height(100.dp))
-                    Text(text = "Dattebayo", modifier = Modifier.clickable {
-                        Toast.makeText(this@MainActivity, "Dattebayo", Toast.LENGTH_SHORT).show()
-                    })
+                    Text(text = "Dattebayo", modifier = Modifier
+                        .clickable {
+                            Toast
+                                .makeText(this@MainActivity, "Dattebayo", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        .background(Color.White)
+                        .border(2.dp, Color.Black)
+                        .padding(20.dp)
+                    )
                 }
             }
         }
--- app/src/main/java/com/e/learningcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/e/learningcompose/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun LearningComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun LearningComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
