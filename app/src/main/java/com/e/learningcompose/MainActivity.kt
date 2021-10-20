2c77662328ad541b8f1534409e4a464438e535fc
AtulYadav01 <ashuy033@gmail.com>
2021-10-20 21:50:41 +0530

    New Functionality Added.

--- app/src/main/java/com/e/learningcompose/MainActivity.kt
+++ app/src/main/java/com/e/learningcompose/MainActivity.kt
@@ -94,11 +94,6 @@ fun Greeting(name: String) {
 }
 
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
